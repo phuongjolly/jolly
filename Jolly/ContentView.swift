13b44449a4494aaca0d9c60cd95dd87eb741422c
phuongjolly <lephuong06t3@gmail.com>
2025-03-17 14:52:54 -0700

    Initial Commit

---
+++ Jolly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jolly
+//
+//  Created by Phuong Le Thi on 2025-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jolly/JollyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JollyApp.swift
+//  Jolly
+//
+//  Created by Phuong Le Thi on 2025-03-17.
+//
+
+import SwiftUI
+
+@main
+struct JollyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
